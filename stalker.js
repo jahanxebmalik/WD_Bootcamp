54775f7f0722669199374f76f4acfb08e1d6a1e5
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-02 23:24:12 +0500

    JS Stalker Exercise

---
+++ stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is you first Name?");
+var lastName = prompt("What is last name?");
+console.log("Your full name is " + firstName + " " + lastName);
+var age = prompt("How old are you?");
+console.log("You are " + age + " years old")
