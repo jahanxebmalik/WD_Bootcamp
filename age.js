61c36774767cde419bcfe90dadc956df088b1205
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-03 00:18:31 +0500

    Age Calculator JS

---
+++ age.js
@@ -0,0 +1,3 @@
+var age = prompt("How old are you?");
+var ageDays = age * 365.25;
+alert(age + " years is roughly " + ageDays + " days");
--- stalker.js
+++ stalker.js
@@ -1,5 +1,6 @@
 var firstName = prompt("What is you first Name?");
 var lastName = prompt("What is last name?");
-console.log("Your full name is " + firstName + " " + lastName);
+var fullName = firstName + " " + lastName;
+console.log("Your full name is " + fullName);
 var age = prompt("How old are you?");
 console.log("You are " + age + " years old")
