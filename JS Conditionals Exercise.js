b167a945da125abbb02ead6a38460d1abec4f1c7
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-04 00:37:30 +0500

    JS Conditional Exercise

--- JS Conditionals Exercise.js
+++ JS Conditionals Exercise.js
@@ -1,4 +1,5 @@
-var age = prompt("What is your age?");
+// Get age and convert it to a Number (prompt always returns a String)
+var age = Number(prompt("What is your age?"));
 if (age < 18) {
     console.log("Sorry, you are not old enough to enter the venue");
 } else if (age < 21) {
@@ -6,11 +7,21 @@ if (age < 18) {
 } else {
     console.log("Come on in, you can drink")
 }
-
+// If age is negative
 if (age < 0) {
     console.log("Error! you have entered incorrect age!");
 }
-if (age == 21) {
+// If age is 21
+if (age === 21) {
     console.log("Happy 21st Birthday!");
 }
-if (age)
+// If age is odd
+//(not evenly divisible by two)
+if (age % 2 !== 0) {
+    console.log("Your age is odd");
+}
+
+// If age is a perfect square
+if (age % Math.sqrt(age) === 0) {
+    console.log("Your age is a perfect square!");
+}
