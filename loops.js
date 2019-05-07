5bf6e10c47d71b22a324ce77a9cd82cbaaa7abfb
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-07 16:56:22 +0500

    While loops

---
+++ loops.js
@@ -0,0 +1,12 @@
+var count = 10;
+while (count <= 20) {
+    console.log("count is: " + count);
+    count++
+}
+
+var str = "jahanzeb";
+var pount = 0;
+while (pount < str.length) {
+    console.log(str[pount]);
+    pount++;
+}
