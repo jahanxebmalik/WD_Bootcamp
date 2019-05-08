9fa5995a2e33144c22c7239f5edad3a3cb88a3f7
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-08 18:43:07 +0500

    Loops JS

--- loops.js
+++ loops.js
@@ -10,3 +10,17 @@ while (pount < str.length) {
     console.log(str[pount]);
     pount++;
 }
+
+var num = 1;
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+}
+
+var num2 = 1;
+while (num2 <= 20) {
+    if (num2 % 4 === 0) {
+        console.log(num2);
+    }
+    num2++;
+}
