37d536ac1a37d2fba799c03109ec3620a0db670e
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-09 21:48:28 +0500

    For loop Instroduction

---
+++ forLoop.js
@@ -0,0 +1,10 @@
+//Printing number from 1 to 5
+for (var count = 1; count < 6; count++) {
+    console.log("count is " + count);
+}
+
+//Printing each character in a string with a for loop
+var str = "hello";
+for (i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
