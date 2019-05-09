56744f510639580a52884d1beb6a9836a2d2e7a4
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-09 17:08:40 +0500

    While Loops exercise

--- whileLoops.js
+++ whileLoops.js
@@ -31,7 +31,7 @@ while (odd < 334) {
 console.log("PRINT ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
 var counter = 5;
 while (counter <= 50) {
-    if (counter / 3) {
+    if (counter % 5 === 0 && counter % 3 === 0) {
         console.log(counter);
     }
     counter += 1;
