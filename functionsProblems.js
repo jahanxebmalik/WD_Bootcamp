d399a37ead9225cba9f18b48e887a536188522bb
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-11 16:39:04 +0500

    function for isEven

---
+++ functionsProblems.js
@@ -0,0 +1,6 @@
+// Write a function isEven which takes a single numeric argument and returns true if the number is event, and false otherwise
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else return false;
+}
