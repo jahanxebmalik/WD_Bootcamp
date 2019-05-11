b8dc3b1acfc1d9460dd51a8b28ac5279d757f9fc
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-11 16:28:10 +0500

    JS functions Intro Exercises

---
+++ functionsIntro.js
@@ -0,0 +1,15 @@
+// Exercise 2
+function test(x, y) {
+    return y - x;
+}
+
+test(10, 40); //30
+
+// Exercise 2
+function test(x) {
+    return x * 2;
+    console.log(x);
+    return x / 2;
+}
+
+test(40); //80
