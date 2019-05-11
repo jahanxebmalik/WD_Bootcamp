35c08a9f3833293d6cd44b551192a4daefbe1506
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-11 17:05:27 +0500

    kebab-case to snake_case function js

--- functionsProblems.js
+++ functionsProblems.js
@@ -19,3 +19,14 @@ factorial(5); //120
 factorial(2); //2
 factorial(10); //3628800
 factorial(0); //1
+
+// Write a function kebabToSnake() which takes a single kebab-cased string argument and returns the snake_cased version
+// Basically, replace "-"s with "_"s
+
+function kebabToSnake(str) {
+    //replace all dashes with
+    var myString = str.replace(/-/g, "_");
+    //return str
+    return myString;
+    //try with: "this - is -a - ---test";
+}
