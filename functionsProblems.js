3e5dc3841a5f88659ccc2b77bad468e47548be48
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-11 16:53:26 +0500

    factorial function in JS

--- functionsProblems.js
+++ functionsProblems.js
@@ -4,3 +4,18 @@ function isEven(num) {
         return true;
     } else return false;
 }
+
+isEven(4); //true
+// Write a function () which takes a single numeric argument and returns the factorial of that number
+function factorial(num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+factorial(5); //120
+factorial(2); //2
+factorial(10); //3628800
+factorial(0); //1
