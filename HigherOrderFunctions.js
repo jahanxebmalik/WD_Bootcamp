ed87a2ac5f34349950d13dedc5582b212b8e0870
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-13 00:17:51 +0500

    Higher order functions

---
+++ HigherOrderFunctions.js
@@ -0,0 +1,7 @@
+function sing() {
+    console.log("Twinkle twinkle little star");
+    console.log("How I wonder...");
+}
+
+// setInterval(sing, 1000) //setInterval (anotherFunc, Interval)
+clearInterval(22);
