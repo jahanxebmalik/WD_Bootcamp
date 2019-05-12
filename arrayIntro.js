4edc08a7bd6deac008273fa2cce946d8c8500c3f
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-13 00:53:05 +0500

    Intro to Arrays

---
+++ arrayIntro.js
@@ -0,0 +1,17 @@
+// Concatenate
+var friends = ["Charlie", "Liz", "David", "Mattias"];
+console.log(friends[0] + " <3 " + friends[1]);
+
+// To update data in array
+friends[0] = "Chuck";
+
+// To add new data in array
+friends[4] = "Amelie";
+
+// We can initialize an empty Array two way
+// 1. var friends = [];
+// 2. var friends = newArray();
+
+// Array has a length property
+var nums = [45, 37, 89, 24];
+nums.length //4
