12cd490e83a03f4af6a2d24518cf48091e644004
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-14 00:36:07 +0500

    Array Exercises JS

---
+++ arrayProblems.js
@@ -0,0 +1,10 @@
+// var numbers = [22, 67, 33, 96, 88];
+// console.log(numbers[numbers.length]);
+
+var friendGroups = [
+    ["Harry", "Ron", "Hermione"],
+    ["Malfoy", "Crabbe", "Goyle"],
+    ["Mooney", "Wormtail", "Prongs"],
+]
+
+console.log(friendGroups[2][0]);
