20c8df503d99412fcf4757215f2434ce3f2d1b81
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-15 17:43:30 +0500

    JS Problem sets

---
+++ JSArrayProblemSet.js
@@ -0,0 +1,10 @@
+var reverse = [];
+
+function printReverse(input) {
+    for (var i = input.length - 1; i >= 0; i--) {
+        reverse.push(input[i]);
+    }
+    reverse.forEach(function (reverse)) {
+        console.log(reverse);
+    }
+}
