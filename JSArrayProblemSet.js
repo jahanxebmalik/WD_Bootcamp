e74085550a34cb1d89d039353ab08ec34cc3d475
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-15 22:51:36 +0500

    Array JS sum, max,  isUniform, reverse

--- JSArrayProblemSet.js
+++ JSArrayProblemSet.js
@@ -32,9 +32,26 @@ function isUniform(arr) {
 // sumArray ([10,3,10,4]);   //27
 // sumArray ([-5,100]);      //95
 
+function sumArray(arr) {
+    var total = 0;
+    arr.forEach(function (element) {
+        total += element;
+    });
+    return total;
+}
 
 // max()
 // Write a function max() that accepts an array of numbers and returns the maximum number in the array
 // max([1,2,3]);       //3
 // max([10,3,10,4]);   //10
 // max([-5,100]);       //100
+
+function max(arr) {
+    var max = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
