6e09647d9a3b2a2781cb0d8912f7af740acbeec1
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-16 21:17:42 +0500

    JS Objects Exercise

--- objectExercise.js
+++ objectExercise.js
@@ -9,4 +9,24 @@ someObject.age = 6; //valid
 var prop = "color"
 
 someObject[prop] = "red"; //valid
-someObject .123 = true; //invalid
+// someObject.123 = true; //invalid
+
+// Exercie 2
+var someObject = {
+    frineds: [{
+            name: "Malfoy"
+        },
+        {
+            name: "Crabbe"
+        },
+        {
+            name: "Goyle"
+        }
+    ],
+    color: "baby blue",
+    isEvil: true
+};
+
+// Write code to retrieve "Malfoy" from someObject 
+someObject.friends[0].name
+// Go one "layer" at a time!
