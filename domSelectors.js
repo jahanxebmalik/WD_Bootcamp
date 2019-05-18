cb4b5f7ef41b025318696f9eb32580abae5674bb
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-18 22:35:05 +0500

    DOM Selector

--- domSelectors.js
+++ domSelectors.js
@@ -11,3 +11,9 @@ console.log(tagHeading[0]);
 // Returns the first element that matches a given CSS-style selector
 var tagQuery = document.querySelector("#highlight");
 console.log(tagQuery);
+
+var tagClass = document.querySelector(".bolded");
+console.log(tagClass);
+
+var tagAll = document.querySelectorAll(".bolded");
+console.log(tagAll);
