8dd10050674d9a0092587965a4f0d459cb6c8e03
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-18 20:18:58 +0500

    Changing background using DOM

--- domIntro.js
+++ domIntro.js
@@ -2,3 +2,15 @@
 var h1 = document.querySelector("h1");
 // Manipulate using <h1> we selected
 h1.style.color = "pink";
+
+var body = document.querySelector("body"); //Select
+var isBlue = false;
+
+setInterval(function () { //Manipulate
+    if (isBlue) {
+        body.style.background = "white";
+    } else {
+        body.style.background = "#3498db";
+    }
+    isBlue = !isBlue;
+}, 1000);
