13a3ee7fcc4160925afe45b52da38b0c5eed42b6
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-22 18:06:22 +0500

    Toggling between bg through eventListener

---
+++ eventExercise.js
@@ -0,0 +1,38 @@
+// Exercise 1
+// Change color of background after clicking on button
+
+// var button = document.querySelector("button");
+// var body = document.querySelector("body");
+// var isGreen = false;
+// -------------------------------------------------------
+// button.addEventListener("click", changeColor);
+
+// function changeColor() {
+//     body.style.background = "purple";
+// }
+// --------------------------------------------------------
+// Another way
+// button.addEventListener("click", function () {
+//     if (isGreen) {
+//         body.style.background = "white";
+//     } else {
+//         body.style.background = "green";
+//     }
+//     isGreen = !isGreen;
+// })
+
+// ---------------------------------------------------------
+// Colt's way of toggling between bg color
+
+var button = document.querySelector("button");
+var isPurple = false;
+
+button.addEventListener("click", function () {
+    if (isPurple) {
+        document.body.style.background = "white";
+        isPurple = false;
+    } else {
+        document.body.style.background = "purple";
+        isPurple = true;
+    }
+})
