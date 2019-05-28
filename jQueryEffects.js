d29bd0958ca7dac3d26fca1d1f1147a0deb450c6
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-29 00:40:01 +0500

    jQuery fade and slide effects

--- jQueryEffects.js
+++ jQueryEffects.js
@@ -1,5 +1,33 @@
+// fadeOut() div when button is clicked
+// $("button").on("click", function () {
+//     $("div").fadeOut(1000, function () {
+//         $(this).remove();
+//     });
+// });
+
+// fadeIn() div when button is clicked //display: none
+// $("button").on("click", function () {
+//     $("div").fadeIn(1000);
+// });
+
+// fadeToggle()
+// $("button").on("click", function () {
+//     $("div").fadeToggle(500);
+// })
+
+// slideDown() //display: none
+// $("button").on("click", function () {
+//     $("div").slideDown(500);
+// })
+
+// slideUp()
+// $("button").on("click", function () {
+//     $("div").slideUp(500);
+// })
+
+// slideToggle()
 $("button").on("click", function () {
-    $("div").fadeOut(1000, function () {
-        $(this).remove();
-    });
-});
+    $("div").slideToggle(1000, function () {
+        // $(this).remove();
+    })
+})
