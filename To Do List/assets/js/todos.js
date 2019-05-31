57c9d101aaa7bc35c6e02ccc2b9cf557359a9431
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-05-31 18:24:11 +0500

    todos remove with fadeOut

--- To Do List/assets/js/todos.js
+++ To Do List/assets/js/todos.js
@@ -1,21 +1,13 @@
 // check off specific todos by clicking
 $("li").click(function () {
-    $(this).classToggle("completed");
-    // console.log($(this).css("color"));
-    //if li is gray
-    // if ($(this).css("color") === "rgb(128, 128, 128)") {
-    //     // turn it black
-    //     $(this).css({
-    //         color: "black",
-    //         textDecoration: "none"
-    //     });
-    //     //else
-    // } else {
-    //     // turn it gray
-    //     $(this).css({
-    //         color: "gray",
-    //         textDecoration: "line-through"
-    //     });
-    // }
-
+    $(this).toggleClass("completed");
 });
+
+//Click on X to delete todos
+$("span").click(function (event) {
+    //this will remove span, parent() will remove complete li
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
