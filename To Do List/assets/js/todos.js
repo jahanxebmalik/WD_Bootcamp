98c961158d54bfd6b3096fdc7b536ac7bf601000
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-06-01 16:03:40 +0500

    todo js functionality completed

--- To Do List/assets/js/todos.js
+++ To Do List/assets/js/todos.js
@@ -1,26 +1,42 @@
 // Select li and change color to gray with line-through
-$("li").click(function () {
+$("ul").on("click", "li", (function () {
     $(this).toggleClass("completed");
-})
+}))
 
-// Click on X and remove li
-$("span").click(function (event) {
+// OR
+// console.log($(this).css("color"));
+// if li is gray
+// if ($(this).css("color") === "rgb(128, 128, 128)") {
+//     // turn it black
+//     $(this).css({
+//         color: "black",
+//         textDecoration: "none"
+//     });
+//     //else
+// } else {
+//     // turn it gray
+//     $(this).css({
+//         color: "gray",
+//         textDecoration: "line-through"
+//     });
+// }
+
+//Click on X to delete todos
+// $("span").click(function (event) {
+$("ul").on("click", "span", (function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+}))
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        // to clear todo from input field
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 })
-
-// check off specific todos by clicking
-// $("li").click(function () {
-//     $(this).toggleClass("completed");
-// });
-
-// //Click on X to delete todos
-// $("span").click(function (event) {
-//     //this will remove span, parent() will remove complete li
-//     $(this).parent().fadeOut(500, function () {
-//         $(this).remove();
-//     });
-//     event.stopPropagation();
-// })
