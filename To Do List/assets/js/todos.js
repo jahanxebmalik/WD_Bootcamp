ab9b23323c91807d7192712f8b88f9d317bcaeae
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-06-01 15:32:48 +0500

    line-through and deleting todos

--- To Do List/assets/js/todos.js
+++ To Do List/assets/js/todos.js
@@ -1,13 +1,26 @@
-// check off specific todos by clicking
+// Select li and change color to gray with line-through
 $("li").click(function () {
     $(this).toggleClass("completed");
-});
+})
 
-//Click on X to delete todos
+// Click on X and remove li
 $("span").click(function (event) {
-    //this will remove span, parent() will remove complete li
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
 })
+
+// check off specific todos by clicking
+// $("li").click(function () {
+//     $(this).toggleClass("completed");
+// });
+
+// //Click on X to delete todos
+// $("span").click(function (event) {
+//     //this will remove span, parent() will remove complete li
+//     $(this).parent().fadeOut(500, function () {
+//         $(this).remove();
+//     });
+//     event.stopPropagation();
+// })
